37a4e8d50e03cca23ff2af5b79baf13ff64dd750
spacecaker <robin-barry@hotmail.com>
2013-07-19 17:03:39 +0200

    see proper changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,113 +12,113 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# config.mk
-#
-# Product-specific compile-time definitions.
-#
-
+# Call this directory in order to use files
 LOCAL_PATH:= $(call my-dir)
 
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
-# for now
-USE_CAMERA_STUB := true
-WITH_DEXPREOPT := true
+#Some configs
+
+USE_CAMERA_STUB:= true
+COPYBIT_MSM7K := true
+
+# Bootloader/Radio
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+#Test
+TARGET_NO_INITLOGO := true
 
-TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/cooper/recovery_kernel
-TARGET_RECOVERY_INITRC := device/samsung/cooper/recovery.rc
-
+# CPU
 TARGET_BOARD_PLATFORM := msm7k
 TARGET_ARCH_VARIANT := armv6-vfp
 TARGET_CPU_ABI := armeabi-v6l
 TARGET_CPU_ABI2 := armeabi
 
+# GPU
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-TARGET_BOOTLOADER_BOARD_NAME := cooper
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+BOARD_EGL_CFG := device/samsung/gio/prebuilt/system/lib/egl/egl.cfg
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
 
-# assert
-TARGET_OTA_ASSERT_DEVICE := cooper,GT-S5830
+# OTA infos
+TARGET_BOOTLOADER_BOARD_NAME := gio
+TARGET_OTA_ASSERT_DEVICE := gio,GT-S5660
 
+# Video/audio
+TARGET_OVERLAY_ALWAYS_DETERMINES_FORMAT := true
+TARGET_USE_SOFTWARE_AUDIO_AAC := true
+TARGET_PROVIDES_LIBAUDIO := true
+
+# Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-
-# FM Radio
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-BOARD_FM_DEVICE := bcm4325
-#BOARD_USE_BROADCOM_FM_VOLUME_HACK := true
+BOARD_FORCE_STATIC_A2DP := true
 HAS_BCM20780 := true
 BOARD_GLOBAL_CFLAGS += -DHAS_BCM20780
 
-# Wifi related defines
+# Wifi
 BOARD_WPA_SUPPLICANT_DRIVER := AWEXT
 BOARD_WLAN_DEVICE           := ar6000
-WIFI_DRIVER_MODULE_PATH     := "/system/wifi/ar6000.ko"
+WIFI_DRIVER_MODULE_PATH     := /system/wifi/ar6000.ko
 WIFI_DRIVER_MODULE_ARG      := ""
 WIFI_DRIVER_MODULE_NAME     := ar6000
 
+# JIT/JS
 WITH_JIT := true
 ENABLE_JSC_JIT := true
-WITH_DEXPREOPT := true
-
-TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-
 JS_ENGINE := v8
+BOARD_USE_SCREENCAP:= true
 
-# OpenGL drivers config file path
-BOARD_EGL_CFG := device/samsung/cooper/egl.cfg
-
-# No fallback font by default (space savings)
-#NO_FALLBACK_FONT:=true
-
-BOARD_GPS_LIBRARIES := libloc_api
-
-BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QCOM_LIBS := true
-BOARD_USES_QCOM_GPS := true
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := cooper
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
-
+#Old string ->TARGET_KERNEL_SOURCE := kernel/samsung/msm7x27
+#BOARD_MOBILEDATA_INTERFACE_NAME:= "pdp0"
+#New one -> thanks phiexz
+#Now have to test
 BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
 
+# FM
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := bcm2049
+
+# Kernel infos
+BOARD_NAND_PAGE_SIZE := 4096 -s 128
+BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x13600000
 BOARD_KERNEL_PAGESIZE := 4096
+BOARD_PAGE_SIZE := 0x00001000
 
-TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true 
+#TARGET_KERNEL_SOURCE := kernel/samsung/msm7x27
+#TARGET_KERNEL_CONFIG := cyanogenmod_gio_defconfig
 
-TARGET_PROVIDES_LIBRIL := true
-TARGET_PROVIDES_LIBAUDIO := true
+# Partitions info
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 219938816
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 190054400
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BML_BOOT := "/dev/block/bml8"
+BOARD_BML_RECOVERY := "/dev/block/bml9"
+BOARD_RECOVERY_HANDLES_MOUNT := true
+BOARD_HAS_DOWNLOAD_MODE := true
 
-# USB
+# Some recovery/system prebuilts
+TARGET_PREBUILT_KERNEL := device/samsung/gio/prebuilt/kernel
+TARGET_RECOVERY_INITRC := device/samsung/gio/prebuilt/recovery.rc
+TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/gio/prebuilt/recovery_kernel
+
+# Files needed for correct recovery config
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/gio/recovery/recovery_ui.c
+BOARD_CUSTOM_GRAPHICS           := ../../../device/samsung/gio/recovery/graphics.c
+
+# USB Mass Storage
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/usb_mass_storage/gadget/lun"
 BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
 
-# # cat /proc/mtd
-# dev:    size   erasesize  name
-# mtd0: 00480000 00020000 "recovery"
-# mtd1: 00480000 00020000 "boot"
-# mtd2: 00180000 00020000 "splash"
-# mtd3: 00060000 00020000 "misc"
-# mtd4: 02940000 00020000 "cache"
-# mtd5: 0cf80000 00020000 "system"
-# mtd6: 0d020000 00020000 "userdata"
-# mtd7: 00180000 00020000 "persist"
-
-
-# Recovery
-TARGET_USERIMAGES_USE_EXT4         := true
-BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x00480000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00780000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 536870912
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE             := 131072
-
-BOARD_BML_BOOT                     := "/dev/block/bml8"
-BOARD_BML_RECOVERY                 := "/dev/block/bml9"
-
-BOARD_RECOVERY_HANDLES_MOUNT := true
-
-BOARD_CUSTOM_RECOVERY_KEYMAPPING   := ../../device/samsung/cooper/recovery/recovery_ui.c
-BOARD_CUSTOM_GRAPHICS              := ../../../device/samsung/cooper/recovery/graphics.c
+# Qualcomm stuff
+BOARD_GPS_LIBRARIES := libloc_api
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_GPS := true		
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := gio
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
+BOARD_GPS_NEEDS_XTRA := true
